38e5f6885a24bb2845627cbb055a93d0d891375e
sneakyxzje <tuandattobi@gmail.com>
2026-05-15 02:37:54 +0700

    feat: update test method to return UserResource response

--- server/app/Http/Controllers/Api/UserController.php
+++ server/app/Http/Controllers/Api/UserController.php
@@ -3,23 +3,25 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    /**
-     * @return JsonResponse
-     */
+
     public function test(): JsonResponse
     {
-        return response()->json([
-            'success' => true,
-            'message' => 'Success',
-            'server_time' => now()->format('Y-m-d H:i:s'),
-            'environment' => app()->environment(),
-            'app_name' => config('app.name', 'Vivu Booking API')
-        ]);
+        // Mock
+        $dummy = new User();
+        $dummy->id = 888;
+        $dummy->name = 'Vivu Booking';
+        $dummy->email = 'vivu@services.com';
+        $dummy->created_at = now();
+
+        return $this->success(
+            new UserResource($dummy),
+            'Success.'
+        );
     }
- 
 }
