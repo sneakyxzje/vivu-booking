e2792bed134df391c79952f99f6de88b2eda5a43
nta0811 <anhntph49412@gmail.com>
2026-05-25 14:56:22 +0700

    theanh

--- server/app/Http/Middleware/RoleMiddleware.php
+++ server/app/Http/Middleware/RoleMiddleware.php
@@ -4,23 +4,14 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class RoleMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next, ...$roles): Response
+    public function handle(Request $request, Closure $next, $role)
     {
-        $user = $request->user();
-
-        if (!$user || !in_array($user->role, $roles)) {
+        if (!$request->user() || $request->user()->role !== $role) {
             return response()->json([
-                'success' => false,
-                'message' => 'Access Denied!.'
+                'message' => 'Forbidden - no permission'
             ], 403);
         }
 
