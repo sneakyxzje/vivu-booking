88e1d3daf494e615f906b1d81ad1b4690ea2dbae
sneakyxzje <tuandattobi@gmail.com>
2026-06-28 13:18:37 +0700

    fix: duplicate import

--- server/app/Http/Controllers/Api/AuthController.php
+++ server/app/Http/Controllers/Api/AuthController.php
@@ -6,8 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
-use Illuminate\Support\Facades\Hash;
-use App\Models\User;
+
 
 class AuthController extends Controller
 {
